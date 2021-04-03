f619a6f60c2750575df4bd9000c8b9060e39aac2
Ian Kipchirchir <potterke4@gmail.com>
2021-04-03 05:05:05 +0300

    Enable broswer syncing

--- webpack.mix.js
+++ webpack.mix.js
@@ -11,5 +11,11 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
+mix.js('resources/js/app.js', 'public/js').vue()
     .sass('resources/sass/app.scss', 'public/css');
+
+if (mix.inProduction()) {
+	mix.version();
+}
+
+mix.browserSync('http://127.0.0.1:8000');
