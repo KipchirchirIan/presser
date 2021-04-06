1c32cdeb2a91df535d60605c2f47027a226ecc39
Ian Kipchirchir <potterke4@gmail.com>
2021-04-06 12:17:28 +0300

    Add route names

--- routes/web.php
+++ routes/web.php
@@ -24,11 +24,11 @@ Route::get('/admin/dashboard', function () {
 })->name('dashboard');
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'] ,function () {
-    Route::get('/posts', 'PostController@index');
-    Route::get('/posts/create', 'PostController@create');
-    Route::post('/posts', 'PostController@store');
-    Route::get('/posts/{post}', 'PostController@show');
-    Route::get('/posts/{post}/edit', 'PostController@edit');
-    Route::put('/posts/{post}', 'PostController@update');
-    Route::delete('/posts/{post}', 'PostController@destroy');
+    Route::get('/posts', 'PostController@index')->name('post.index');
+    Route::get('/posts/create', 'PostController@create')->name('post.create');
+    Route::post('/posts', 'PostController@store')->name('post.store');
+    Route::get('/posts/{post}', 'PostController@show')->name('post.show');
+    Route::get('/posts/{post}/edit', 'PostController@edit')->name('post.edit');
+    Route::put('/posts/{post}', 'PostController@update')->name('post.update');
+    Route::delete('/posts/{post}', 'PostController@destroy')->name('post.destroy');
 });
